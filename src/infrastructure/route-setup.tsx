8c16aae3867eed1836160965cca1a89d2ff666b8
CitouHub <218735@skane.se>
2025-05-10 23:09:02 +0200

    first commit

--- src/infrastructure/route-setup.tsx
+++ src/infrastructure/route-setup.tsx
@@ -2,25 +2,13 @@ import HomeIcon from '@mui/icons-material/Home';
 import ConstructionIcon from '@mui/icons-material/Construction';
 import {
     ROUTE_PATH_WELCOME,
-    ROUTE_PATH_CONSTRUCTION_PROJECT,
-    ROUTE_PATH_RISK_ASSESSMENT,
-    ROUTE_PATH_CUSTOMER_ORDER,
-    ROUTE_PATH_CUSTOMER_ORDER_ADD_RISK,
     ROUTE_PATH_TRACK
 } from "./route";
 import { RouteCategory } from './route-category';
 import Welcome from '../section/-welcome/welcome';
-import ConstructionProjectList from '../section/-construction-project/construction-project-list';
-import RiskAssessmentHandler from '../section/-risk-assessment/risk-assessment-handler';
-import RiskAssessmentCustomHandler from '../section/-risk-assessment-add/risk-assessment-add-handler';
-import CustomerOrderHandler from '../section/-customer-order/customer-order-handler';
 import Track from '../track';
 
 export const ROUTE_CATEGORY_WELCOME_TITLE = "Start";
-export const ROUTE_CATEGORY_CONSTRUCTION_PROJECT_TITLE = "Byggprojekt";
-export const ROUTE_CATEGORY_RISK_ASSESSMENT_TITLE = "Riskbedömning";
-export const ROUTE_CATEGORY_CUSTOMER_ORDER_TITLE = "Kundorder";
-export const ROUTE_CATEGORY_CUSTOMER_ORDER_ADD_RISK_TITLE = "Lägg till egen risk";
 export const ROUTE_CATEGORY_TRACK_TITLE = "Gissa låten";
 
 export const ROUTE_SETUP: RouteCategory[] = [
@@ -32,50 +20,6 @@ export const ROUTE_SETUP: RouteCategory[] = [
                 path: ROUTE_PATH_WELCOME,
                 icon: <HomeIcon />,
                 component: <Welcome />
-            }
-        ]
-    },
-    {
-        title: ROUTE_CATEGORY_CONSTRUCTION_PROJECT_TITLE,
-        routes: [
-            {
-                name: ROUTE_CATEGORY_CONSTRUCTION_PROJECT_TITLE,
-                path: ROUTE_PATH_CONSTRUCTION_PROJECT,
-                icon: <ConstructionIcon />,
-                component: <ConstructionProjectList />
-            }
-        ]
-    },
-    {
-        title: ROUTE_CATEGORY_RISK_ASSESSMENT_TITLE,
-        routes: [
-            {
-                name: ROUTE_CATEGORY_RISK_ASSESSMENT_TITLE,
-                path: ROUTE_PATH_RISK_ASSESSMENT,
-                icon: <ConstructionIcon />,
-                component: <RiskAssessmentHandler />
-            }
-        ]
-    },
-    {
-        title: ROUTE_CATEGORY_CUSTOMER_ORDER_TITLE,
-        routes: [
-            {
-                name: ROUTE_CATEGORY_CUSTOMER_ORDER_TITLE,
-                path: ROUTE_PATH_CUSTOMER_ORDER,
-                icon: <ConstructionIcon />,
-                component: <CustomerOrderHandler />
-            }
-        ]
-    },
-    {
-        title: ROUTE_CATEGORY_CUSTOMER_ORDER_ADD_RISK_TITLE,
-        routes: [
-            {
-                name: ROUTE_CATEGORY_CUSTOMER_ORDER_ADD_RISK_TITLE,
-                path: ROUTE_PATH_CUSTOMER_ORDER_ADD_RISK,
-                icon: <ConstructionIcon />,
-                component: <RiskAssessmentCustomHandler />
             }
         ]
     },
--- src/infrastructure/route.tsx
+++ src/infrastructure/route.tsx
@@ -1,18 +1,4 @@
-export const PARAMETER_CONSTRUCTION_PROJECT_ID = ":constructionProjectId";
-export const PARAMETER_CUSTOMER_ORDER_ID = ":customerOrderId";
 export const PARAMETER_TRACK_INDEX = ":trackIndex";
 
 export const ROUTE_PATH_WELCOME = "/";
-export const ROUTE_PATH_CONSTRUCTION_PROJECT = "/construction-project";
-export const ROUTE_PATH_RISK_ASSESSMENT = `/risk/assessment/construction-project/${PARAMETER_CONSTRUCTION_PROJECT_ID}`;
-export const ROUTE_PATH_CUSTOMER_ORDER = `/customer/order/${PARAMETER_CUSTOMER_ORDER_ID}`;
-export const ROUTE_PATH_CUSTOMER_ORDER_ADD_RISK = `/customer/order/add/risk/${PARAMETER_CUSTOMER_ORDER_ID}`;
 export const ROUTE_PATH_TRACK = `/track/${PARAMETER_TRACK_INDEX}`;
-
-export interface CustomerOrderParam {
-    customerOrderId: string
-}
-
-export interface ConstructionProjectParam {
-    constructionProjectId: string
-}
