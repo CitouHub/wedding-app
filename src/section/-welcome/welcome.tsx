be105b522f4461750634944bdda970d7540061d4
CitouHub <218735@skane.se>
2025-05-10 23:21:13 +0200

    1

--- src/section/-welcome/welcome.tsx
+++ src/section/-welcome/welcome.tsx
@@ -1,10 +1,4 @@
-import { Button } from "@mui/material";
-import { useNavigate } from "react-router-dom";
-import { ROUTE_PATH_CONSTRUCTION_PROJECT } from "../../infrastructure/route";
-
 export default function Welcome() {
-    const navigate = useNavigate();
-
     return (
         <div>
             <h2>Välkommen!</h2>
@@ -12,13 +6,6 @@ export default function Welcome() {
             <p>Varför behöver jag göra en risk bedömning?<br />Detta är något som din kommun vill ha. De vill veta att du tänkt till kring riskerna som finns med det bygge som du planera. Vi hjälper både dig, och din kommun, bra va!?</p>
             <br />
             <br />
-            <Button
-                variant="contained"
-                style={{ width: '150px'}}
-                onClick={() => navigate(ROUTE_PATH_CONSTRUCTION_PROJECT)}
-            >
-                Välj projekt
-            </Button>
         </div>
     );
 }
