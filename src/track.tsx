22e2257705cdb76b431de68d8139a65f9fce255b
CitouHub <218735@skane.se>
2025-05-13 22:16:22 +0200

    Updates result text to amin streigth for Leo

--- src/track.tsx
+++ src/track.tsx
@@ -188,15 +188,18 @@ export default function Track() {
         <div className='wrapper'>
             <div className='item'>
                 {trackIndex !== undefined && <div className="item-content">
-                    <audio controls src={track.file} />
+                    <audio controls autoPlay src={track.file} />
                     <div className="item-form">
                         {guessScore !== undefined && <div style={{ maxWidth: '300px', paddingTop: '1rem' }}>
                             Poäng totalt: {getTotalScore()}
-                            {getTotalScore() === 9 && <p>GODE GUD!!! Sjukt imponerande! Fan vilket kejsarpar ni är! Jag bugar framförer er djupt</p>}
-                            {getTotalScore() <= 8 && getTotalScore() >= 6 && <p>Fantastisk! Akhila, jag är glad att du hittad Leo</p>}
-                            {getTotalScore() <= 5 && getTotalScore() >= 4 && <p>Snyggt! Ni är gryma tillsamans, jag blir glad varje gång jag ser er!</p>}
-                            {getTotalScore() <= 3 && getTotalScore() >= 1 && <p>...nja MYCKET mer än hade jag väntat mig. Akhila ge Leo lite smisk!</p>}
-                            {getTotalScore() === 0 && <p>Men... Men... Leo och Akhila... FAN va besviken jag är...</p>}
+                            {getTotalScore() === 9 && <p>GODE GUD!!! Sjukt imponerande! Jag bugar framförer inför dig, så grym hade jag ALDRIG trott att du skulle vara. Bara EN person är bättre, Akhila ;)</p>}
+                            {getTotalScore() <= 8 && getTotalScore() >= 7 && <p>DET! VAR! SOM! FAM! Du är ju helt HELT fantastisk då detta, mycket bättre än jag trott kära vän!</p>}
+                            {getTotalScore() <= 7 && getTotalScore() >= 6 && <p>Fantastisk! Nu ligger du på en nivå som får mig att känna att är förtjänt av ett liv med Akhila!</p>}
+                            {getTotalScore() <= 6 && getTotalScore() >= 5 && <p>Ohhhh snyggt! Men jag trodde du du skulle vara liiiite bättre, kom igen!</p>}
+                            {getTotalScore() <= 5 && getTotalScore() >= 4 && <p>Nice! Men inte super nice, du kan bättre!</p>}
+                            {getTotalScore() <= 4 && getTotalScore() >= 3 && <p>Inte HELT värdelöst, meeeeen ganska, jag vet ju att du är bättre än såhär. Jaja, skyll på stavningen bara...</p>}
+                            {getTotalScore() <= 2 && getTotalScore() >= 1 && <p>...Det där var ju inte så bra, jag trodde du skulle vara MYCKET bättre!</p>}
+                            {getTotalScore() === 0 && <p>Men... Men... Leo... FAN va besviken jag är... Kommer du inte ihåg våra klubbår? :( Tar du en 0:a till så åker jag fan hem igen! Inget vittne och ingen Rikard!</p>}
                         </div>}
                         <TextField
                             type='text'
